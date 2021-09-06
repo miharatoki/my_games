15003fa4f6e4e03520392402f9a6fc9346f5f2e6
miharatoki <kou566533@gmail.com>
2021-09-06 02:17:56 +0000

    make sign_up and sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,12 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    new_user_registration_path
+    new_user_session_path
   end
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
-    devise_parameter_sanitizer.permit(:sign_in,keys:[:email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
   end
 end
