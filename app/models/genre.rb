f8f9490ddba5529e94a4de5100bd9dd1e4df7bc6
miharatoki <kou566533@gmail.com>
2021-09-07 06:15:18 +0000

    create posts_index

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,7 +2,7 @@ class Genre < ApplicationRecord
   has_many :posts, dependent: :destroy
 
   def self.select_array
-    genres_array = []
+    genres_array = Array.new
     self.all.each do |genre|
       genres_array << [genre.name, genre.id]
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :users, only: [:index, :show, :edit, :update] do
+    get 'users/:id/genres/:id' => 'genres#show'
+  end
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
