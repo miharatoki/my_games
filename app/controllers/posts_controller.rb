30bda13d1287087f0d08b843ae00f66c442a978a
miharatoki <kou566533@gmail.com>
2021-09-07 12:38:05 +0000

    fix score_star

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
-  before_action :set_genre_array, only: [:new, :create, :edit, :update]
-  before_action :set_post,        only: [:show, :edit]
+  before_action :set_genre_array,     only: [:new, :create, :edit, :update]
+  before_action :ensure_posted_user,  only: [:edit, :update, :destroy]
+  
 
   def new
     @post = Post.new
@@ -11,6 +12,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
@@ -30,19 +32,24 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    
   end
 
   private
   def post_params
     params.require(:post).permit(:genre_id, :title, :body, :total_score, :story_score, :graphic_score, :operability_score, :sound_score, :balance_score)
   end
+  
+  def ensure_posted_user
+    @post = Post.find(params[:id])
+    unless @post.user_id = current_user.id
+      flash[:alert] = '権限がないため処理できません。'
+      redirect_to :posts_path
+    end
+  end
 
   def set_genre_array
     @genre_array = Genre.select_array
-  end
-
-  def set_post
-    @post = Post.find(params[:id])
   end
 
 end
