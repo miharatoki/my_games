5f651bfc557933210c7269b9cd74029e402f16ef
miharatoki <kou566533@gmail.com>
2021-09-10 03:56:22 +0000

    users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
+  before_action :ensure_user, only: [:show, :edit, :update]
+  
   def index
   end
 
   def show
+    
   end
 
   def edit
   end
+  
+  def update
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image_id)
+  end
+  
+  def set_user
+    @user = User.find(current_user.id)
+  end
+  
+  def ensure_user
+    redirect_to request.referer unless params[:id] = current_user.id
+  end
+    
 end
