fa68ad8411adf8ee3411be6088e3a7f3549d43f8
miharatoki <kou566533@gmail.com>
2021-09-10 04:06:15 +0000

    change method users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,21 @@
 class UsersController < ApplicationController
+  before_action :ensure_user, only: [:edit, :update]
   before_action :set_user, only: [:show, :edit]
-  before_action :ensure_user, only: [:show, :edit, :update]
   
   def index
+    @users = User.all
   end
 
   def show
-    
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    
   end
   
   private
@@ -20,12 +23,12 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :introduction, :profile_image_id)
   end
   
-  def set_user
-    @user = User.find(current_user.id)
-  end
-  
   def ensure_user
     redirect_to request.referer unless params[:id] = current_user.id
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
     
 end
