f8adb13e093b46a4a44bda0c796660cd216a707d
miharatoki <kou566533@gmail.com>
2021-09-20 07:26:16 +0000

    create model_test

---
+++ spec/models/notification_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe 'Notificationモデルのテスト' do
+  let!(:post) {create(:post)}
+  let!(:favorite) {create(:favorite, post_id: post.id)}
+  let!(:post_comment) {create(:post_comment, post_id: post.id)}
+  let!(:favorite_notification) {create(:notification, favorite_id: favorite.id, action: 'favorite')}
+  let!(:post_comment_notification) {create(:notification, post_comment_id: post_comment.id, action: 'post_comment')}
+  
+  
+
+  context '親モデルのレコードが削除された時に紐づけられたnotificationのレコードも削除されるか' do
+    it '全件取得' do 
+      expect(Notification.all.count).to eq 2
+    end
+    
+    # Postモデルと直接紐づいていないためPostモデルと直接紐づいているPostCommentモデルPKを使用
+    it 'Postモデルのレコードを削除した時(post_comment.id)' do
+      post.destroy
+      expect(Notification.find_by(post_comment_id: post_comment.id)).to eq nil
+    end
+    
+    # FavoriteモデルもPostモデルと直接紐づいているため、同じようにテスト
+    it 'Postモデルのレコードを削除した時(favorite.id)' do
+      post.destroy
+      expect(Notification.find_by(favorite_id: favorite.id)).to eq nil
+    end
+    
+    # いいねを解除した時
+    it 'Favoriteモデルのレコードを削除した時' do
+      favorite.destroy
+      expect(Notification.all.count).to eq 1
+      expect(Notification.find_by(favorite_id: favorite_notification.favorite_id)).to eq nil
+    end
+    
+    # 投稿へのコメントを消した時
+    it 'PostCommentモデルのレコードを削除した時' do
+      post_comment.destroy
+      expect(Notification.all.count).to eq 1
+      expect(Notification.exists?(post_comment_id: post_comment_notification.post_comment_id)).to eq false
+    end
+  end
+  
+  context 'アソシエーション' do
+    
+    it 'favoriteモデルと多対1の関係である' do
+      expect(Notification.reflect_on_association(:favorite).macro).to eq :belongs_to
+    end
+    
+    it 'post_commentモデルと多対1の関係である' do
+      expect(Notification.reflect_on_association(:post_comment).macro).to eq :belongs_to
+    end
+  end
+
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,95 @@
+require 'rails_helper'
+
+RSpec.describe 'Postモデルのテスト', type: :model do
+  subject {post.valid?}
+  let!(:post) {build(:post)}
+
+  context '空白で保存した時のバリデーション'do
+    it 'titleを空白で保存できないこと' do
+      post.title = ''
+      is_expected.to eq false
+      expect(post.errors[:title]).to include('が入力されていません。')
+    end
+
+    it 'bodyを空白で保存できないこと' do
+      post.body = ''
+      is_expected.to eq false
+      expect(post.errors[:body]).to include('が入力されていません。')
+    end
+
+    it 'total_scoreを空白で保存できないこと白' do
+      post.total_score = ''
+      is_expected.to eq false
+      expect(post.errors[:total_score]).to include('が入力されていません。')
+    end
+
+    it 'story_scoreを空白で保存できないこと' do
+      post.story_score = ''
+     is_expected.to eq false
+      expect(post.errors[:story_score]).to include('が入力されていません。')
+    end
+
+    it 'graphic_scoreを空白で保存できないこと' do
+      post.graphic_score = ''
+      is_expected.to eq false
+      expect(post.errors[:graphic_score]).to include('が入力されていません。')
+    end
+
+    it 'operability_scoreを空白で保存できないこと' do
+      post.operability_score = ''
+      is_expected.to eq false
+      expect(post.errors[:operability_score]).to include('が入力されていません。')
+    end
+
+    it 'sound_scoreを空白で保存できないこと' do
+      post.sound_score = ''
+      is_expected.to eq false
+      expect(post.errors[:sound_score]).to include('が入力されていません。')
+    end
+
+    it 'balance_scoreを空白で保存できないこと' do
+      post.balance_score = ''
+     is_expected.to eq false
+      expect(post.errors[:balance_score]).to include('が入力されていません。')
+    end
+  end
+
+  context 'titleとbodyの文字制限のバリデーション' do
+
+    it 'titleを30文字以上で保存しようとした時はエラー' do
+      post = Post.new(title: Faker::Lorem.characters(number:31))
+      expect(post).to be_invalid
+      expect(post.errors[:title]).to include('は30文字以内で入力してください')
+    end
+
+    it 'bodyを400文字以上で保存しようとした時はエラー' do
+      post = Post.new(body: Faker::Lorem.characters(number:401))
+      expect(post).to be_invalid
+      expect(post.errors[:body]).to include('は400文字以内で入力してください')
+    end
+
+    it 'titleを30文字ちょうどで保存しようとした時は保存できること' do
+      post.title = Faker::Lorem.characters(number:30)
+      expect(post).to be_valid
+    end
+
+    it 'bodyを400文字ちょうどで保存しようとした時は保存できること' do
+      post.body = Faker::Lorem.characters(number:400)
+      expect(post).to be_valid
+    end
+  end
+
+  context 'アソシエーション' do
+    it 'Userモデルと多対1の関係であること' do
+      expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
+    end
+
+    it 'Genreモデルと多対1の関係であること' do
+      expect(Post.reflect_on_association(:genre).macro).to eq :belongs_to
+    end
+
+    it 'PostCommentモデルと1対多の関係であること' do
+      expect(Post.reflect_on_association(:post_comments).macro).to eq :has_many
+    end
+  end
+end
