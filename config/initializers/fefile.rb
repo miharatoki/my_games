6426bf8aa763adfb42771cdbde70bf094abb61fb
miharatoki <kou566533@gmail.com>
2021-10-06 12:37:33 +0000

    fix refile.rb

--- config/initializers/fefile.rb
+++ config/initializers/fefile.rb
@@ -1,11 +1,11 @@
 require 'refile/s3'
 
-if Rails.env.production?
+if Rails.env.production? 
   aws = {
-    access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキーID
-    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキー
-    region: 'ap-northeast-1',
-    bucket: 'my-games-files',
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION'],
+    bucket: ENV['AWS_S3_BUCKET_NAME']
   }
   Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
   Refile.store = Refile::S3.new(prefix: 'store', **aws)
