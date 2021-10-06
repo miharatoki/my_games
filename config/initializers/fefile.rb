d6983484f1d3a82a9e5ad5ab5a30518051ec6fe0
miharatoki <kou566533@gmail.com>
2021-10-06 13:28:47 +0000

    fix refile.rb

--- config/initializers/fefile.rb
+++ config/initializers/fefile.rb
@@ -1,6 +1,6 @@
 require 'refile/s3'
 
-if Rails.env.production? 
+
   aws = {
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
@@ -9,4 +9,4 @@ if Rails.env.production?
   }
   Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
   Refile.store = Refile::S3.new(prefix: 'store', **aws)
-end
+
